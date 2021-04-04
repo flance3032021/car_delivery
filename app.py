87c6a339bdf0df48da34089cbe9a26a7eec507f1
flance3032021 <81904813+flance3032021@users.noreply.github.com>
2021-04-04 16:35:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,31 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 total_price= 0
 
-transport_type= st.selectbox('Select type of transport : ', ('Private Dyna','Dyna/bed/Dyna','Lowbed','Luxury','Motocycle'))
-
-insurance= st.checkbox('I need insurance.')
-  
-if transport_type in ['Private Dyna','Luxury','Motocycle']:
-  if insurance:
-    price_per_km= 1.7
-  else:
-    price_per_km= 1
+private_dyna= st.number_input('Enter number of Private Dyna', min_value= 0, max_value= 100, step= 1)
+if st.checkbox('I need insurance.', key= '1'):
+  private_dyna_price_per_km= 1.7
 else:
-  price_per_km= 0.85
+  private_dyna_price_per_km= 1  
+
+dyna_bed_dyna= st.number_input('Enter number of Dyna/bed/Dyna', min_value= 0, max_value= 100, step= 1)
+st.checkbox('I need insurance.', key= '2')
+dyna_bed_dyna_price_per_km= 0.85
+
+lowbed= st.number_input('Enter number of Lowbed', min_value= 0, max_value= 100, step= 1)
+st.checkbox('I need insurance.', key= '3')
+lowbed_price_per_km= 0.85
+
+luxury= st.number_input('Enter number of Luxury', min_value= 0, max_value= 100, step= 1)
+if st.checkbox('I need insurance.', key= '4'):
+  luxury_price_per_km= 1.7
+else:
+  luxury_price_per_km= 1  
+
+motocycle= st.number_input('Enter number of Motocycle', min_value= 0, max_value= 100, step= 1)
+if st.checkbox('I need insurance.', key= '5'):
+  motocycle_price_per_km= 1.7
+else:
+  motocycle_price_per_km= 1  
 
 col1, col2= st.beta_columns(2)
 time1= col1.time_input('Order time : ')
@@ -39,45 +53,44 @@ try:
   loc2= geolocator.geocode(place2)
   lat_lon1= (loc1.latitude, loc1.longitude)
   lat_lon2= (loc2.latitude, loc2.longitude)
-  distt= geodesic(lat_lon1, lat_lon2).kilometers
-  st.write(f'The distance is {round(distt, 2)} kilometres.')
+  private_dyna_distt= geodesic(lat_lon1, lat_lon2).kilometers
+  dyna_bed_dyna_distt= private_dyna_distt
+  lowbed_distt= private_dyna_distt
+  luxury_distt= private_dyna_distt
+  motocycle_distt= private_dyna_distt
+  
+  st.write(f'The distance is {round(private_dyna_distt, 2)} kilometres.')
 
-  if transport_type=='Private Dyna':
-    if distt>50.0:
-      total_price= (distt*price_per_km)+100
-    else:
-      total_price= 150
+  if private_dyna_distt>50.0:
+    total_price= total_price+(((private_dyna_distt*private_dyna_price_per_km)+100)*private_dyna)
+  else:
+    total_price= total_price+(150*private_dyna)
 
-  if transport_type=='Dyna/bed/Dyna':
-    if distt>300.0:
-      total_price= (distt*price_per_km)+300
-    else:
-      total_price= 0
+  if dyna_bed_dyna_distt>300.0:    
+    total_price= total_price+(((dyna_bed_dyna_distt*dyna_bed_dyna_price_per_km)+300)*dyna_bed_dyna)    
+  else:
+    total_price= total_price+(0*dyna_bed_dyna)
+  
+  if lowbed_distt>300.0:
+    total_price= total_price+(((lowbed_distt*lowbed_price_per_km)+300)*lowbed)
+  else:
+    total_price= total_price+(0*lowbed)
 
-  if transport_type=='Lowbed':
-    if distt>300.0:
-      total_price= (distt*price_per_km)
-    else:
-      total_price= 0
+  if luxury_distt>149.0:
+    total_price= total_price+((((luxury_distt*luxury_price_per_km)+(750000*0.01)))*luxury)
+  else:
+    total_price= total_price+(((luxury_distt*luxury_price_per_km)+(750000*0.005))*luxury)
 
-  if transport_type=='Luxury':
-    if distt>149.0:
-      total_price= (distt*price_per_km)+(750,000*0.01)
-    else:
-      total_price= (distt*price_per_km)+(750,000*0.005)
+  if motocycle_distt>50.0:
+    total_price= total_price+(((motocycle_distt*motocycle_price_per_km)+100)*motocycle)
+  else:
+    total_price= total_price+(150*motocycle)
 
-  if transport_type=='Motocycle':
-    if distt>50.0:
-      total_price= (distt*price_per_km)+100
-    else:
-      total_price= 150
-
-  st.subheader(f'Total cost of transportation : {total_price}')
+  st.subheader(f'Total cost of transportation : {round(total_price, 2)}')  
   st.number_input('Insert your price')
 
 except:
   pass
-
 
 
 
